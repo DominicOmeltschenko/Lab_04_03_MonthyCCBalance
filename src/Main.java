6c24526970de9688008bf8fd14e925f1478898d1
Dominic <Domdombossy@gmail.com>
2024-09-19 14:27:39 -0400

    Updated with extra code. UNTESTED!

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        
+        double creditCardBalance = 5000.00;
+        final double INTEREST_RATE = 0.17;
+        double firstMonth = 0.00;
+        double secondMonth = 0.00;
+
+        firstMonth = creditCardBalance + creditCardBalance * INTEREST_RATE;
+        secondMonth = creditCardBalance + creditCardBalance * INTEREST_RATE;
+
+        System.out.println("Your interest with a credit card balance of: " + creditCardBalance + " and a monthly interest rate of: " + INTEREST_RATE + " is: " + firstMonth + " for one month, and: " + secondMonth + " for two months.");
+
     }
 }
